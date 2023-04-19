81d71a1fca07694ec43ca288d4f63b2b3538b290
saadturky <saad.turky2000@gmail.com>
2023-04-19 19:47:00 +0300

    Adding gun reload, vision cone material and fixing player movements

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,22 +5,24 @@ public class Controller : MonoBehaviour {
 
 	public float moveSpeed = 6;
 
-	Rigidbody rigidbody;
+	Rigidbody rigid;
 	Camera viewCamera;
 	Vector3 velocity;
 
 	void Start () {
-		rigidbody = GetComponent<Rigidbody>();
+		rigid = GetComponent<Rigidbody>();
 		viewCamera = Camera.main;
 	}
 
 	void Update () {
 		Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
-		transform.LookAt (mousePos + Vector3.up * transform.position.y);
-		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * moveSpeed;
+        //transform.LookAt (mousePos + Vector3.up * transform.position.y);
+        mousePos.y = transform.position.y;
+        transform.LookAt(mousePos);
+        velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * moveSpeed;
 	}
 
 	void FixedUpdate() {
-		rigidbody.MovePosition (rigidbody.position + velocity * Time.fixedDeltaTime);
+		rigid.MovePosition (rigid.position + velocity * Time.fixedDeltaTime);
 	}
 }
---
+++ Assets/Scripts/PlayerShootingWithReloading.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingWithReloading : MonoBehaviour
+{
+    //public GameObject bulletDestroyPrefab;
+    public GameObject bulletPrefab;
+    public float range = 5f;
+    public Transform fireSpawnPoint;
+    public float fireRate = 0.25f;
+    public float bulletSpeed = 20;
+    private float nextBullet;
+    public ParticleSystem muzzleFlash;
+    private Animator playerAnimator;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
+    [Header("UI Elements")]
+    public TMPro.TextMeshProUGUI ammoText;
+
+    public void Awake()
+    {
+        //playerAnimator = GetComponent<Animator>();
+        currentAmmo = maxAmmo;
+    }
+
+    private void Update()
+    {
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0f)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextBullet)
+        {
+            nextBullet = Time.time + fireRate;
+            Shoot();
+        }
+
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+        //animator.SetBool("Reloading", true);
+        yield return new WaitForSeconds(reloadTime - .25f);
+        //animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(.25f);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
+
+    void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            muzzleFlash.Play();
+            currentAmmo--;
+            ammoText.SetText(currentAmmo.ToString()+ "/"+ maxAmmo.ToString());
+            GameObject bullet = Instantiate(bulletPrefab, fireSpawnPoint.position, Quaternion.identity);
+            Destroy(bullet, 2f);
+            bullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            //playerAnimator.SetBool("Fire", false);
+        }
+
+    }
+}
