1c69ec8c9ea64e344d42d02fd69471b410b0b966
saadturky <saad.turky2000@gmail.com>
2023-06-05 18:11:51 +0300

    Game Over screen fading added

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,10 +6,21 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     public GameObject gameOverMenu;
+    bool isPaused;
+    public Texture2D BoxBorder;
+
+    public FadeInOut GameOverFade;
+
 
     private void Start()
     {
-        gameOverMenu.SetActive(false);
+        //gameOverMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    private void Update()
+    {
+        PauseGame();
     }
 
     private void OnEnable()
@@ -24,11 +35,19 @@ public class UIManager : MonoBehaviour
 
     public void EnablegameOverMenu()
     {
-        gameOverMenu.SetActive(true);
+        //gameOverMenu.SetActive(true);
+        GameOverFade.FadeIn();
     }
 
-    public void RestartLevel()
+    public void RestartGame()
     {
+        StartCoroutine(RestartLevel());
+    }
+
+    IEnumerator RestartLevel()
+    {
+        GameOverFade.FadeOut();
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
@@ -36,6 +55,34 @@ public class UIManager : MonoBehaviour
     {
         UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
+    }
+
+    void PauseGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) )
+        {
+            isPaused = !isPaused;
+            Time.timeScale = 0;
+            if (!isPaused)
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+
+    void OnGUI()
+    {
+        GUIStyle headStyle = new GUIStyle();
+
+        if (isPaused)
+        {
+            GUI.Label(new Rect(11, 100, 330, 315), BoxBorder, headStyle);
+        }
+        else
+        {
+            GUI.Label(new Rect(11, 100, 330, 315), "", headStyle);
+        }
     }
 
 }
